1a6d98b274c1df05a6532414f87729235cb39d54
fahreza <fahreza@bitcorp.id>
2025-06-22 21:14:39 +0700

    update navbar and about

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -12,16 +12,34 @@ const Circle = ({text,background,hover,icon}) => {
     )
 }
 
-const Line = ({text,background,hover}) => {
+
+
+const Line = ({text,hover,textColor,textColorHover}) => {
     return(
         <>
-            <button className={`flex`}>
-
+            <button className={`
+                flex items-center justify-center text-[18px] gap-2 cursor-pointer
+                transition-colors ease-in-out duration-300
+                ${hover} /* Biarkan prop hover di sini jika ada hover state lain untuk button itu sendiri */
+                ${textColor} /* Terapkan warna teks default di button */
+                group /* Tambahkan class 'group' di sini */
+            `}>
+                <p className={`
+                    underline underline-offset-8 decoration-3
+                    transition-colors ease-in-out duration-300
+                    ${textColorHover}/* Tambahkan group-hover untuk warna teks */
+                `}>
+                    {text}
+                </p>
+                <div className={`
+                    font-extrabold rotate-230 text-3xl
+                    transition-colors ease-in-out duration-300
+                    ${textColorHover}/* Tambahkan group-hover untuk warna ikon */
+                `}>
+                    <ion-icon name="arrow-down-outline"></ion-icon>
+                </div>
             </button>
         </>
     )
-
-
 }
-
 export {Circle, Line}
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,14 +1,14 @@
 import photo from "../assets/Photo.png";
-import {Circle} from "../components/Button/Button"
+import { Circle, Line } from "../components/Button/Button";
 const Introduction = () => {
   return (
     <>
       <section>
         {/* container */}
-        <div className="px-[20px] md:px-[50px] lg:px-[122px] flex justify-between items-center">
+        <div className="w-full px-[20px] md:px-[50px] lg:px-[80px] xl:px-[122px] flex flex-col lg:flex-row lg:justify-between items-center mt-10 mb-10">
           {/* Content Left */}
-          <div className="w-1/2 items-center justify-center">
-            <div className="">
+          <div className="w-1/2 md:w-full items-center justify-center flex ">
+            <div className="flex-col gap-2.5 flex">
               {/* name */}
               <div className="">
                 <h3 className="font-bold text-[20px]">Hello, I’m </h3>
@@ -17,28 +17,38 @@ const Introduction = () => {
                 </h1>
               </div>
               {/* description */}
-              <div className="">
+              <div className="mb-2.5">
                 <p className="text-[18px] text-justify">
                   I am graduate of Telkom University majoring in Informatics
-                  Engineering, with expertise in the fields of DevOps Engineer,lffdevops/blog-kubernetes
-                  Computer Networking, IT Infrastructure and System Analysist. I
-                  want to have a career in IT, and I am committed to continuing
-                  to grow and deliver innovative solutions.
+                  Engineering, with expertise in the fields of DevOps
+                  Engineer,lffdevops/blog-kubernetes Computer Networking, IT
+                  Infrastructure and System Analysist. I want to have a career
+                  in IT, and I am committed to continuing to grow and deliver
+                  innovative solutions.
                 </p>
               </div>
               {/* Button */}
-              <div className="flex bg-color-">
+              <div className="inline-flex gap-10 ">
+                <div className="">
                 <Circle
                   text="Download CV"
                   background="bg-primary"
                   icon="cloud-download"
                 />
+                </div>
+                <div className="flex">
+                  <Line
+                    text="View Portofolio"
+                    textColorHover="group-hover:text-black"
+                    textColor="text-primary"
+                  />
+                </div>
               </div>
             </div>
           </div>
 
           {/* Content Right */}
-          <div className="w-1/2 flex items-center justify-center">
+          <div className="w-1/2 md:w-full flex items-center justify-center">
             <div className="">
               <img src={photo} alt="photo landing" />
             </div>
