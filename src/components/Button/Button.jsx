d09602096dd00a5e16f0f85d19d816637994f381
fahreza <fahreza@bitcorp.id>
2025-09-05 14:02:32 +0700

    feat: add function download

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,25 +1,30 @@
-const Circle = ({text,background,hover,icon}) => {
-    return(
-        <>
-            <button
-      className={`
-        inline-flex items-center 
-        px-5 sm:px-7 py-2 sm:py-2.5 
-        rounded-lg sm:rounded-full 
-        gap-1.5 sm:gap-2.5 
-        cursor-pointer 
-        hover:bg-dark transition-colors ease-in-out duration-300
-        ${background} ${hover} text-base sm:text-lg md:text-[18px]
-      `}
-    >
-                <p className="font-extrabold text-white">{text}</p>
-                <div className="text-white">
-                    <ion-icon name={icon}></ion-icon>
-                </div>
-            </button>
-        </>
-    )
-}
+const Circle = ({ text, background, hover, icon, file }) => {
+  const classes = `
+    inline-flex items-center 
+    px-5 sm:px-7 py-2 sm:py-2.5 
+    rounded-lg sm:rounded-full 
+    gap-1.5 sm:gap-2.5 
+    cursor-pointer 
+    hover:bg-dark transition-colors ease-in-out duration-300
+    ${background} ${hover} text-base sm:text-lg md:text-[18px]
+  `;
+
+  return file ? (
+    <a href={file} download className={classes}>
+      <p className="font-extrabold text-white">{text}</p>
+      <div className="text-white">
+        <ion-icon name={icon}></ion-icon>
+      </div>
+    </a>
+  ) : (
+    <button className={classes}>
+      <p className="font-extrabold text-white">{text}</p>
+      <div className="text-white">
+        <ion-icon name={icon}></ion-icon>
+      </div>
+    </button>
+  );
+};
 
 
 
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -11,30 +11,35 @@ const Introduction = () => {
             <div className="flex-col gap-2.5 flex">
               {/* name */}
               <div className="">
-                <h3 className="font-bold text-lg sm:text-xl md:text-2xl">Hello, I’m </h3>
+                <h3 className="font-bold text-lg sm:text-xl md:text-2xl">
+                  Hello, I’m{" "}
+                </h3>
                 <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl lg:text-[44px]">
                   Landewank Fahreza Firdaus
                 </h1>
               </div>
               {/* description */}
               <div className="mb-2.5">
-                <p className="text-[18px] text-justify">
+                <p className=" text-justify">
                   I am graduate of Telkom University majoring in Informatics
-                  Engineering, with expertise in the fields of DevOps
-                  Engineer, Computer Networking, IT
-                  Infrastructure and System Analysist. I want to have a career
-                  in IT, and I am committed to continuing to grow and deliver
-                  innovative solutions. I always uphold the values of discipline, work order, collaboration ability, one-one cycle work, and a sense of responsibility in every task I carry out.
+                  Engineering, with expertise in the fields of DevOps Engineer,
+                  Computer Networking, IT Infrastructure and System Analysist. I
+                  want to have a career in IT, and I am committed to continuing
+                  to grow and deliver innovative solutions. I always uphold the
+                  values of discipline, work order, collaboration ability,
+                  one-one cycle work, and a sense of responsibility in every
+                  task I carry out.
                 </p>
               </div>
               {/* Button */}
-              <div className="inline-flex gap-10 ">
+              <div className="inline-flex gap-5 sm:gap-8 md:gap-10 flex-wrap">
                 <div className="">
-                <Circle
-                  text="Download CV"
-                  background="bg-primary"
-                  icon="cloud-download"
-                />
+                  <Circle
+                    text="Download CV"
+                    background="bg-primary"
+                    icon="cloud-download"
+                    file="/Documents/CV-DevOps-Infrastructure.pdf"
+                  />
                 </div>
                 <div className="flex">
                   <Line
