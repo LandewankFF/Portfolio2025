7a3f1236951c48d27b40f26fe937442bf128d1b2
fahreza <fahreza@bitcorp.id>
2025-12-05 15:26:37 +0700

    Add data service

--- src/data/DataService.js
+++ src/data/DataService.js
@@ -1,9 +1,5 @@
 // src/data/DataService.js  
-import { 
-  Workflow as devops,
-  Server, Activity
-} from 'lucide-react';
-
+import { Workflow as devops, Server, Activity } from "lucide-react";
 const DataService = [
   {
     id: 1,
@@ -18,20 +14,20 @@ const DataService = [
     id: 2,
     title: "Server Configuration and Administration",
     description:
-      "Handles configuration, troubleshooting , and maintenance of Linux -based systems. and server.",
+      "Handles configuration, troubleshooting , and maintenance of Linux-based systems and servers.",
     iconSrc: Server,
     aos: "fade-up",
     delay: 1200,
   },
-
   {
     id: 3,
     title: "Infrastructure Monitoring & Logging",
     description:
-      "Set up a monitoring system using tools like Grafana and Prometheus to track the health and performance of applications and infrastructure.",
+      "Set up monitoring systems using Grafana and Prometheus to track health and performance.",
     iconSrc: Activity,
     aos: "fade-up",
     delay: 1700,
-  }
+  },
 ];
+
 export default DataService;
